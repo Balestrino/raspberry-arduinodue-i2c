1563be9bd4d1144d12a6444da530829ddc8b83d3
balestrino <salvatore.balestrino@gmail.com>
2019-07-03 10:38:47 +0100

    Rpi code

---
+++ README.md
@@ -0,0 +1,1 @@
+# RPi-ArduinoDue-I2C
---
+++ Rpi/index.js
@@ -0,0 +1,24 @@
+const { RaspiIO } = require('raspi-io');
+const five = require('johnny-five');
+const board = new five.Board({
+  io: new RaspiIO()
+});
+
+const readline = require('readline');
+
+board.on('ready', function () {
+  // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
+  const led = new five.Led('P1-7');
+  led.strobe(500);
+  
+  console.log("sending i2c..");
+  var write = (message) => {
+    this.i2cWrite(0x08, Array.from(message, c => c.charCodeAt(0)));
+  };
+  this.i2cConfig();
+  this.repl.inject({ write });
+
+  write("Hello World");
+
+});
+
